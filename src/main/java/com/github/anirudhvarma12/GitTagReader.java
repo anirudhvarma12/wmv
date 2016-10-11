bde24a49c6d5e27a74b15e4dc49d5fde16662851
Anirudh Varma <varma93@hotmail.com>
2016-10-11 09:33:11 +0530

    Fixes #3, use findGitDir instead of setGitDir to support nested folders

--- src/main/java/com/github/anirudhvarma12/GitTagReader.java
+++ src/main/java/com/github/anirudhvarma12/GitTagReader.java
@@ -13,16 +13,11 @@ public class GitTagReader {
 
 	public static String getCurrentTag(String path) throws IOException {
 		FileRepositoryBuilder builder = new FileRepositoryBuilder();
-		Repository repository = builder
-				.setGitDir(new File(appendGitFolder(path))).readEnvironment()
-				.findGitDir().build();
+		Repository repository = builder.findGitDir(new File(path))
+				.readEnvironment().build();
 		Map<String, Ref> tagMap = repository.getTags();
 		Set<String> tags = tagMap.keySet();
 		return getLastTag(tags);
-	}
-
-	private static final String appendGitFolder(String path) {
-		return path + File.separator + ".git";
 	}
 
 	private static String getLastTag(Set<String> tags) {
@@ -34,9 +29,4 @@ public class GitTagReader {
 		tags.toArray(tagArray);
 		return tagArray[tags.size() - 1];
 	}
-
-	public static void main(String[] args) throws IOException {
-		System.out.println(getCurrentTag("C:\\workspace\\opensource\\wmv\\"));
-	}
-
 }
